e6044bfdb1d3f9f0eb4779fab35fe6f93fbb398c
Ivo Batistela <irbatistela@gmail.com>
2018-04-10 02:09:59 -0300

    removed first feature examples

--- resource/faturamento.module.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('cw.faturamento', ['cw.core.resources']);
-})();
--- resource/tabelaProdutoFornecedor/tabelaProdutoFornecedor.module.js
+++
@@ -1,4 +0,0 @@
-(function () {
-  'use strict';
-  angular.module('cw.faturamento.tabelaProdutoFornecedor', []);
-})();
